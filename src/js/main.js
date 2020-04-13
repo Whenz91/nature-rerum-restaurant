0c8dbe8cd31a73bba744c1f472dbe6cd0acdc9db
Zoltan Miko <mikozoltan91@gmail.com>
2020-04-13 19:00:33 +0200

    created a restaurant home page where completed the nav, the hero section, the about section. Half completed the menu and testemonials sections

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const tabContent = document.querySelectorAll(".tabcontent");
+const tabLinks = document.querySelectorAll(".tab-nav__link");
+
+for(var i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function() {
+        
+        tabContent.forEach((element) => {
+            element.className = element.className.replace(" active", "");
+        });
+    
+        tabLinks.forEach((element) => {
+            element.className = element.className.replace(" active", "");
+        })
+    
+        document.getElementById(this.dataset.id).className += " active";
+        this.className += " active";
+    });
+}
