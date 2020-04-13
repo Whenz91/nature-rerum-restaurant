4eb1bddad05235eca6ee80c96dd6b652ff95f868
Zoltan Miko <mikozoltan91@gmail.com>
2020-04-13 18:57:06 +0200

    created gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const { watch, series } = require('gulp');
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
+
+function transpile() {
+    return src('src/*.html').pipe(dest('public'));
+}
+
+function cssTranspile(cb) {
+    src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('public/css'));
+
+    cb();
+}
+
+function jsTranspile() {
+    return src('src/js/*.js').pipe(dest('public/js'));
+}
+
+// a figyelés a gulp parancssal inditható a terminálban
+exports.default = function() {
+    watch('src/*.html', transpile);
+    watch('src/sass/*.scss', cssTranspile);
+    watch('src/js/*.js', jsTranspile);
+}
