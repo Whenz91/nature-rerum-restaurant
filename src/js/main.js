164b80e04c21ca8be9b0a3e7f50417974592f7c4
Zoltan Miko <mikozoltan91@gmail.com>
2020-04-14 19:31:39 +0200

    created slider for testemonials and created table reservation section and form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* Tab */
 const tabContent = document.querySelectorAll(".tabcontent");
 const tabLinks = document.querySelectorAll(".tab-nav__link");
 
@@ -16,3 +17,45 @@ for(var i = 0; i < tabLinks.length; i++) {
         this.className += " active";
     });
 }
+
+/* Slider */
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+const nextSlide = () => {
+    //Get current class
+    const current = document.querySelector('.current');
+    //Remove current class
+    current.classList.remove('current');
+    //Check for next slide
+    if(current.nextElementSibling) {
+        //Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        //Add current to start
+        slides[0].classList.add('current');
+    }
+}
+const prevSlide = () => {
+    //Get current class
+    const current = document.querySelector('.current');
+    //Remove current class
+    current.classList.remove('current');
+    //Check for prev slide
+    if(current.previousElementSibling) {
+        //Add current to prev sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        //Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+}
+
+//Button events
+next.addEventListener('click', e => {
+    nextSlide();
+});
+prev.addEventListener('click', e => {
+    prevSlide();
+});
