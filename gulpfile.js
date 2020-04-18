d36e72f29d3ccc052c44ced976d390d4ffb0e362
Zoltan Miko <mikozoltan91@gmail.com>
2020-04-18 16:37:51 +0200

    created mobile menu open-close function

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,11 @@ function transpile() {
     return src('src/*.html').pipe(dest('public'));
 }
 
-function cssTranspile(cb) {
-    src('src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('public/css'));
-
-    cb();
-}
+// function cssTranspile() {
+//     src('src/sass/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(dest('public/css'));
+// }
 
 function jsTranspile() {
     return src('src/js/*.js')
@@ -24,6 +22,6 @@ function jsTranspile() {
 // a figyelés a gulp parancssal inditható a terminálban
 exports.default = function() {
     watch('src/*.html', transpile);
-    watch('src/sass/*.scss', cssTranspile);
+    //watch('src/sass/**/*.scss', cssTranspile);
     watch('src/js/*.js', jsTranspile);
 }
